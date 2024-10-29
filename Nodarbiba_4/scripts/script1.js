c67d3c404f3d9c371d0134ebd48ee44f920586ac
Andrejs-bbb <jtl@inbox.lv>
2024-10-29 22:09:43 +0200

    fff

--- Nodarbiba_4/scripts/script1.js
+++ Nodarbiba_4/scripts/script1.js
@@ -1,2 +1,2 @@
 date2 = Date.now();
-console.log("2. izdruka no head daļas laika momentā -" + date2 + "ms");
+console.log("2. izdruka no head daļas laika momentā - " + date2 + "ms");
--- Nodarbiba_4/scripts/script2.js
+++ Nodarbiba_4/scripts/script2.js
@@ -2,6 +2,6 @@
 
 {
 date4 = Date.now();
-console.log("4. izdruka no body daļas laika momentā -" + date4 + "ms");
+console.log("4. izdruka no body daļas laika momentā - " + date4 + "ms");
 }
 
