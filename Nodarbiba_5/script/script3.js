46c8cd9b4b53ae264030abea9acb736bfa375cc5
Andrejs-bbb <jtl@inbox.lv>
2024-11-02 17:14:46 +0200

    mmm

--- Nodarbiba_5/script/script3.js
+++ Nodarbiba_5/script/script3.js
@@ -2,7 +2,12 @@ N = numbers.length;
 var i = 0;
 
 console.log(numbers);
+console.log(i < N);
 
-while (i < N) { 
-    console.log(i, numbers[i]);
+while (i < N) {
+  console.log(i < N);
+  console.log(i, numbers[i]);
+  i++;
+  console.log(i < N);
+}
     
