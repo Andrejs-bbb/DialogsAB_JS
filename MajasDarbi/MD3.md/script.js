83c12e36b69de5217baf7fda0d8ef3650c7ab5cd
Andrejs <jtl@inbox.lv>
2024-11-12 17:32:02 +0200

    Update script.js

--- MajasDarbi/MD3.md/script.js
+++ MajasDarbi/MD3.md/script.js
@@ -1,12 +1,10 @@
 function count(from, to, choice, exclusions = []) {
     const output = document.getElementById('output');
-    output.innerHTML = ''; // Notīra iepriekšējo rezultātu
+    output.innerHTML = '';
 
     for (let i = from; i <= to; i++) {
-        // Ja skaitlis ir izslēgts, turpinām ar nākamo
         if (exclusions.includes(i)) continue;
 
-        // Izvēles apstrāde
         if (choice === 'all') {
             output.innerHTML += i + ' ';
         } else if (choice === 'odd' && i % 2 !== 0) {
